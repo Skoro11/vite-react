a66f2a479cf58d2d0b1c0f6155725595f1ee7c21
Toni Skoro <skoro.toni01@gmail.com>
2025-01-30 13:08:24 +0100

    New Arrival section finished

--- src/sections/NewArrival.jsx
+++ src/sections/NewArrival.jsx
@@ -1,7 +1,55 @@
+import "./NewArrival.css";
 function NewArrival() {
   return (
     <>
-      <div className="new-arrival-container">New Arrival</div>
+      <div className="new-arrival-container">
+        <div className="grid-container">
+          <div className="item1">
+            <img src="ps5.png" />
+            <ul className="absolute-arrival">
+              <li className="arrival-heading">PlayStation 5</li>
+              <li className="arrival-description">
+                Black and White version of the PS5 coming out on sale.
+              </li>
+              <li>
+                <button className="arrival-btn">Shop Now</button>
+              </li>
+            </ul>
+          </div>
+          <div className="item2">
+            <img src="women.png" />{" "}
+            <ul className="absolute-arrival">
+              <li className="arrival-heading">Women’s Collections</li>
+              <li className="arrival-description">
+                Featured woman collections that give you another vibe.
+              </li>
+              <li>
+                <button className="arrival-btn">Shop Now</button>
+              </li>
+            </ul>
+          </div>
+          <div className="item3">
+            <img src="speakers.png" />
+            <ul className="absolute-arrival">
+              <li className="arrival-heading">Speakers</li>
+              <li className="arrival-description">Amazon wireless speakers</li>
+              <li>
+                <button className="arrival-btn">Shop Now</button>
+              </li>
+            </ul>
+          </div>
+          <div className="item4">
+            <img src="gucci.png" />
+            <ul className="absolute-arrival">
+              <li className="arrival-heading">Perfume</li>
+              <li className="arrival-description">GUCCI INTENSE OUD EDP</li>
+              <li>
+                <button className="arrival-btn">Shop Now</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
