f96bac35cf7d2e148e050f06ced6b1697e0554c6
Toni Skoro <skoro.toni01@gmail.com>
2025-02-14 12:27:36 +0100

    Responsive contact us page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,24 +3,24 @@ import "./Contact.css"
 function ContactPage(){
     return(
        
-        <section className="mg-inline contact-page">
+        <section className="mg-inline contact-page pd-in-30p pd-in-15-mb">
         <div className="links-home"><a className="home-link" href="/">Home</a> / Contact</div>
 
-<div className="contact-container flex mg-bottom-100">
-    <div className="left-contact-container">
+<div className="contact-container flex mg-bottom-100 direction-column-767">
+    <div className="left-contact-container no-display-320-767">
         <ul className="flex space-between direction-column height-100 ">
             <li className="flex align-center bold"><img src="call-us-icon.png" alt="Call us"/><span className="padd-left-5">Call to us</span></li>
-            <li>We are available 24/7, 7 days a week.</li>
+            <li className="no-display-1023">We are available 24/7, 7 days a week.</li>
             <li>Phone: +8801611112222</li>
             <li><hr></hr></li>
             <li className="flex align-center bold "><img src="write-to-us-icon.png" alt="Write to us"/><span className="padd-left-5">Write To US</span></li>
-            <li>Fill out our form and we will contact you within 24 hours.</li>
+            <li className="no-display-1023">Fill out our form and we will contact you within 24 hours.</li>
             <li>Emails: customer@exclusive.com</li>
             <li>Emails: support@exclusive.com</li>
         </ul>
     </div>
     <div className="right-contact-container">
-        <div className="flex space-between">
+        <div className="flex gap-3">
         <input type="text" className="input-contact-page" placeholder="Your Name*" />
         <input type="text" className="input-contact-page" placeholder="Your Email*" />
         <input type="text" className="input-contact-page" placeholder="Your Phone*" />
